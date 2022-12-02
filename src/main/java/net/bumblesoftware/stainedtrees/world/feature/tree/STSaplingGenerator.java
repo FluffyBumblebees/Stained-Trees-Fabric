1daf6d56e0141c2223c5a98a74ab2c4e4b3d0a07
Fluffy Bumblebee <lromanic123alt@gmail.com>
2022-12-03 01:04:27 +1100

    Michael scofield

--- src/main/java/net/bumblesoftware/stainedtrees/world/feature/tree/STSaplingGenerator.java
+++ src/main/java/net/bumblesoftware/stainedtrees/world/feature/tree/STSaplingGenerator.java
@@ -4,20 +4,35 @@ import net.minecraft.block.sapling.SaplingGenerator;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Supplier;
 
 public class STSaplingGenerator extends SaplingGenerator {
-    private final  Supplier<RegistryEntry<? extends ConfiguredFeature<?, ?>>> colour;
-    public STSaplingGenerator(Supplier<RegistryEntry<? extends ConfiguredFeature<?, ?>>> colour) {
-        this.colour = colour;
+    private final Supplier<RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>>> def;
+    private final Supplier<RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>>> defBees;
+    private final Supplier<RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>>> fat;
+    private final Supplier<RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>>> fatBees;
+    public STSaplingGenerator(
+            Supplier<RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>>> def,
+            Supplier<RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>>> defBees,
+            Supplier<RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>>> fat,
+            Supplier<RegistryEntry<ConfiguredFeature<TreeFeatureConfig, ?>>> fatBees
+    ) {
+        this.def = def;
+        this.defBees = defBees;
+        this.fat = fat;
+        this.fatBees = fatBees;
     }
 
     @Nullable
     @Override
     protected RegistryEntry<? extends ConfiguredFeature<?, ?>> getTreeFeature(Random random, boolean bees) {
-        return colour.get();
+        if (random.nextInt(10) == 0) {
+            return bees ? fatBees.get() : fat.get();
+        } else {
+            return bees ? defBees.get() : def.get();
+        }
     }
 }
-
